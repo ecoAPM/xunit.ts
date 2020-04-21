7b65d1a98b4f676aa245fd2c47037c2cac103fc4
Steve Desmond <steve@vtsv.ca>
2020-04-20 21:00:26 -0400

    Add TestRunner class and tests

---
+++ src/TestRunner.ts
@@ -0,0 +1,32 @@
+import TestInfo from './TestInfo';
+import TestSuite from './TestSuite';
+import { TestResult } from './TestResult';
+import ResultReporter from './ResultReporter';
+import { AssertionError } from 'assert';
+
+export default class TestRunner {
+
+    private reporter: ResultReporter;
+
+    public constructor(reporter: ResultReporter) {
+        this.reporter = reporter;
+    }
+
+    public runTest(name: string, info: TestInfo, suite: TestSuite): TestResult {
+        this.reporter.testStarted(suite, name);
+        if (info.value == null) {
+            this.reporter.testIncomplete(suite, name, 0);
+            return TestResult.Incomplete;
+        }
+
+        try {
+            info.value.call(suite);
+            this.reporter.testPassed(suite, name, 0);
+            return TestResult.Passed;
+
+        } catch (error) {
+            this.reporter.testFailed(suite, name, error, 0);
+            return TestResult.Failed;
+        }
+    }
+}
--- tests/TestRunnerTests.ts
+++ tests/TestRunnerTests.ts
@@ -1,5 +1,98 @@
 import { Test, TestSuite } from "../xunit";
+import ResultReporter from "../src/ResultReporter";
+import TestRunner from "../src/TestRunner";
+import Mockito from 'ts-mockito';
+import { TestResult } from "../src/TestResult";
 
 export default class TestRunnerTests extends TestSuite {
+    @Test()
+    public ReturnsTestPassed() {
+        //arrange
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new TestRunner(Mockito.instance(reporter));
 
+        //act
+        const result = runner.runTest('inside reports test passed', { value: () => { } }, new class X extends TestSuite { });
+
+        //assert
+        this.assert.equal(TestResult.Passed, result);
+    }
+
+    @Test()
+    public ReturnsTestFailed() {
+        //arrange
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new TestRunner(Mockito.instance(reporter));
+
+        //act
+        const result = runner.runTest('inside reports test failed', { value: () => { throw new Error(); } }, new class X extends TestSuite { });
+
+        //assert
+        this.assert.equal(TestResult.Failed, result);
+    }
+
+    @Test()
+    public ReturnsTestIncomplete() {
+        //arrange
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new TestRunner(Mockito.instance(reporter));
+
+        //act
+        const result = runner.runTest('inside reports test incomplete', { }, new class X extends TestSuite { });
+
+        //assert
+        this.assert.equal(TestResult.Incomplete, result);
+    }
+
+    @Test()
+    public ReportsTestStarted() {
+        //arrange
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new TestRunner(Mockito.instance(reporter));
+
+        //act
+        runner.runTest('inside reports test started', { value: () => { } }, new class X extends TestSuite { });
+
+        //assert
+        Mockito.verify(reporter.testStarted(Mockito.anything(), Mockito.anything())).once();
+    }
+
+    @Test()
+    public ReportsTestPassed() {
+        //arrange
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new TestRunner(Mockito.instance(reporter));
+
+        //act
+        runner.runTest('inside reports test passed', { value: () => { } }, new class X extends TestSuite { });
+
+        //assert
+        Mockito.verify(reporter.testPassed(Mockito.anything(), Mockito.anything(), Mockito.anything())).once();
+    }
+
+    @Test()
+    public ReportsTestFailed() {
+        //arrange
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new TestRunner(Mockito.instance(reporter));
+
+        //act
+        runner.runTest('inside reports test failed', { value: () => { throw new Error(); } }, new class X extends TestSuite { });
+
+        //assert
+        Mockito.verify(reporter.testFailed(Mockito.anything(), Mockito.anything(), Mockito.anything(), Mockito.anything())).once();
+    }
+
+    @Test()
+    public ReportsTestIncomplete() {
+        //arrange
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new TestRunner(Mockito.instance(reporter));
+
+        //act
+        runner.runTest('inside reports test incomplete', { }, new class X extends TestSuite { });
+
+        //assert
+        Mockito.verify(reporter.testIncomplete(Mockito.anything(), Mockito.anything(), Mockito.anything())).once();
+    }
 }
