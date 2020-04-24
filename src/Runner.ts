168cb5b332b69d04cce971675c1a3e74a8819327
Steve Desmond <steve@vtsv.ca>
2020-04-24 10:03:41 -0400

    Replace runner with testable version

--- src/Runner.ts
+++ src/Runner.ts
@@ -1,84 +1,31 @@
-import { TestSuite } from "../xunit";
-import TestInfo from "./TestInfo";
-import { AssertionError } from "assert";
-import colors from "colors";
-import TestName from "./TestName";
-import fs from "fs";
-import util from "util";
+import TestSuite from "./TestSuite";
+import TestSuiteLoader from './TestSuiteLoader';
+import TestSuiteRunner from './TestSuiteRunner';
+import ResultReporter from "./ResultReporter";
+import TestSuiteResults from "./TestSuiteResults";
 
 export default class Runner {
-    public async runAll(dir: string = './tests'): Promise<void> {
-        const suites = await this.findTestSuites(dir);
-        suites.forEach((suite) => this.runSuite(suite));
+
+    private loader: TestSuiteLoader;
+    private runner: TestSuiteRunner;
+    private reporter: ResultReporter;
+
+    public constructor(loader: TestSuiteLoader, runner: TestSuiteRunner, reporter: ResultReporter) {
+        this.loader = loader;
+        this.runner = runner;
+        this.reporter = reporter;
     }
 
-    public async loadFiles(array: string[], callback: (file_name: string, index: Number, array: string[]) => Promise<void>) {
-        for (let index = 0; index < array.length; index++) {
-            await callback(array[index], index, array);
+    public async runAll(dir: string): Promise<TestSuiteResults[]> {
+        this.reporter.runStarted();
+        const results: TestSuiteResults[] = [];
+        const suites = await this.loader.loadTestSuites(dir);
+        for (let x = 0; x < suites.length; x++) {
+            const suite = suites[x];
+            const result = await this.runner.runSuite(suite);
+            results.push(result);
         }
-    }
-
-    public async findTestSuites(dir: string): Promise<TestSuite[]> {
-        const suites: TestSuite[] = [];
-
-        try {
-            const find_files = util.promisify(fs.readdir);
-            const files = await find_files(dir);
-            await this.loadFiles(files, async (file: string) => {
-                const test_class = await import(`../${dir}/${file.replace('.ts', '')}`);
-                if (!(test_class.default.prototype instanceof TestSuite)) {
-                    console.log(`${file} is not a ${TestSuite.prototype.constructor.name}`);
-                    return;
-                }
-
-                const tests = test_class.default.prototype.getTests();
-                const suite = new test_class.default();
-                suite.setTests(tests);
-                suites.push(suite);
-            });
-        }
-        catch (error) {
-            console.error(error);
-        }
-
-        return suites;
-    }
-
-    public runSuite(suite: TestSuite): void {
-        console.log();
-        console.log(TestName.toSentenceCase(suite.constructor.name));
-        const tests = suite.getTests();
-        if (tests == null || tests.length == 0) {
-            console.log(`  ${colors.yellow('?')} (no tests found)`);
-            return;
-        }
-
-        const test_names = Object.keys(tests);
-        test_names.forEach((name: string) => this.runTest(name, tests[name], suite));
-    }
-
-    public runTest(name: string, info: TestInfo, suite: TestSuite): boolean {
-        try {
-            if (info.value == null) {
-                throw new Error(`Test "${name}" not found`);
-            }
-
-            info.value.call(suite);
-            console.log(`  ${colors.green('✓')} ${name}`);
-            return true;
-        } catch (error) {
-            if (error.name == AssertionError.name) {
-                const assertion_error: AssertionError = error;
-                console.error(`  ${colors.red('✘')} ${name}`);
-                console.error();
-                console.error(`    Expected: ${assertion_error.expected}`);
-                console.error(`      Actual: ${assertion_error.actual}`);
-                console.error();
-            }
-            else {
-                console.error(error);
-            }
-            return false;
-        }
+        this.reporter.runCompleted(results, 0);
+        return results;
     }
 }
---
+++ tests/RunnerTests.ts
@@ -0,0 +1,64 @@
+import { TestSuite, Test } from "../xunit";
+import Runner from "../src/Runner";
+import TestSuiteLoader from "../src/TestSuiteLoader";
+import Mockito from 'ts-mockito';
+import TestSuiteRunner from "../src/TestSuiteRunner";
+import ResultReporter from "../src/ResultReporter";
+import TestSuiteResults from "../src/TestSuiteResults";
+
+export default class RunnerTests extends TestSuite {
+    @Test()
+    public async ReportsRunStarted() {
+        //arrange
+        const loader = Mockito.mock<TestSuiteLoader>();
+        Mockito.when(loader.loadTestSuites(Mockito.anyString())).thenResolve([]);
+        
+        const suite_runner = Mockito.mock<TestSuiteRunner>();
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new Runner(Mockito.instance(loader), Mockito.instance(suite_runner), Mockito.instance(reporter));
+
+        //act
+        await runner.runAll('tests');
+
+        //assert
+        Mockito.verify(reporter.runStarted());
+    }
+
+    @Test()
+    public async ReportsRunCompleted() {
+        //arrange
+        const loader = Mockito.mock<TestSuiteLoader>();
+        Mockito.when(loader.loadTestSuites(Mockito.anyString())).thenResolve([]);
+        
+        const suite_runner = Mockito.mock<TestSuiteRunner>();
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new Runner(Mockito.instance(loader), Mockito.instance(suite_runner), Mockito.instance(reporter));
+
+        //act
+        await runner.runAll('tests');
+
+        //assert
+        Mockito.verify(reporter.runCompleted(Mockito.anything(), Mockito.anything()));
+    }
+
+    @Test()
+    public async ReturnsTestSuiteResults() {
+        //arrange
+        const test_suite_stub = new class X extends TestSuite { };
+
+        const loader = Mockito.mock<TestSuiteLoader>();
+        Mockito.when(loader.loadTestSuites(Mockito.anyString())).thenResolve([test_suite_stub, test_suite_stub]);
+
+        const suite_runner = Mockito.mock<TestSuiteRunner>();
+        Mockito.when(suite_runner.runSuite(Mockito.anything())).thenResolve(new TestSuiteResults(test_suite_stub));
+
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new Runner(Mockito.instance(loader), Mockito.instance(suite_runner), Mockito.instance(reporter));
+
+        //act
+        const results = await runner.runAll('tests');
+
+        //assert
+        this.assert.lengthOf(results, 2);
+    }
+}
