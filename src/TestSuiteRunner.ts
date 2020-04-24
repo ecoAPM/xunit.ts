55b23fafcd41f8aff81a53c4b56f637f5424beef
Steve Desmond <steve@vtsv.ca>
2020-04-24 09:11:16 -0400

    Add test suite runner

---
+++ src/TestSuiteRunner.ts
@@ -0,0 +1,41 @@
+import ResultReporter from './ResultReporter';
+import TestSuite from './TestSuite';
+import TestSuiteResults from './TestSuiteResults';
+import TestInfo from './TestInfo';
+import TestRunner from './TestRunner';
+
+export default class TestSuiteRunner {
+
+    private runner: TestRunner;
+    private reporter: ResultReporter;
+
+    public constructor(runner: TestRunner, reporter: ResultReporter) {
+        this.runner = runner;
+        this.reporter = reporter;
+    }
+
+    public async runSuite(suite: TestSuite): Promise<TestSuiteResults> {
+        this.reporter.suiteStarted(suite);
+        const tests = suite.getTests();
+        const results = await this.runTests(suite, tests);
+        this.reporter.suiteCompleted(suite, results, 0);
+        return results;
+    }
+
+    public async runTests(suite: TestSuite, tests: Record<string, TestInfo>): Promise<TestSuiteResults> {
+        const results = new TestSuiteResults(suite);
+        if (tests == null || Object.keys(tests).length == 0) {
+            this.reporter.testIncomplete(suite, '(no tests found)', 0);
+            return results;
+        }
+
+        const test_names = Object.keys(tests);
+        for (let x = 0; x < test_names.length; x++) {
+            const name = test_names[x];
+            const result = await this.runner.runTest(name, tests[name], suite);
+            results.addResult(name, result);
+        }
+
+        return results;
+    }
+}
---
+++ tests/TestSuiteRunnerTests.ts
@@ -0,0 +1,69 @@
+import { TestSuite, Test } from "../xunit";
+import TestSuiteRunner from "../src/TestSuiteRunner";
+import TestRunner from "../src/TestRunner";
+import Mockito from "ts-mockito";
+import ResultReporter from "../src/ResultReporter";
+import { TestResult } from "../src/TestResult";
+
+export default class TestSuiteRunnerTests extends TestSuite {
+    @Test()
+    public async ReportsSuiteStarted()
+    {
+        //arrange
+        const test_runner = Mockito.mock<TestRunner>();
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new TestSuiteRunner(Mockito.instance(test_runner), Mockito.instance(reporter));
+
+        //act
+        await runner.runSuite(new class X extends TestSuite {});
+
+        //assert
+        Mockito.verify(reporter.suiteStarted(Mockito.anything())).once();
+    }
+
+    @Test()
+    public async ReportsSuiteCompleted()
+    {
+        //arrange
+        const test_runner = Mockito.mock<TestRunner>();
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new TestSuiteRunner(Mockito.instance(test_runner), Mockito.instance(reporter));
+
+        //act
+        await runner.runSuite(new class X extends TestSuite {});
+
+        //assert
+        Mockito.verify(reporter.suiteCompleted(Mockito.anything(), Mockito.anything(), Mockito.anything())).once();
+    }
+
+    @Test()
+    public async ReportsIncompleteIfNoTests()
+    {
+        //arrange
+        const test_runner = Mockito.mock<TestRunner>();
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new TestSuiteRunner(Mockito.instance(test_runner), Mockito.instance(reporter));
+
+        //act
+        await runner.runTests(new class X extends TestSuite {}, {});
+
+        //assert
+        Mockito.verify(reporter.testIncomplete(Mockito.anything(), Mockito.anything(), Mockito.anything())).once();
+    }
+
+    @Test()
+    public async ReturnsResultsFromTestRunner()
+    {
+        //arrange
+        const test_runner = Mockito.mock<TestRunner>();
+        Mockito.when(test_runner.runTest(Mockito.anything(), Mockito.anything(), Mockito.anything())).thenResolve(TestResult.Passed);
+        const reporter = Mockito.mock<ResultReporter>();
+        const runner = new TestSuiteRunner(Mockito.instance(test_runner), Mockito.instance(reporter));
+
+        //act
+        const results = await runner.runTests(new class X extends TestSuite {}, {'test1': {}, 'test2': {}});
+
+        //assert
+        this.assert.equal(2, results.count(TestResult.Passed));
+    }
+}
