d01066a4ad9c4d5296689cec83b2aabf682e58fa
Steve Desmond <steve@vtsv.ca>
2021-05-15 19:55:14 -0400

    Add snake case parsing to test names

--- src/Framework/TestName.ts
+++ src/Framework/TestName.ts
@@ -1,6 +1,9 @@
 export default class TestName {
     static toSentenceCase(test_name: string) {
-        const result = test_name.replace(/([A-Z])/g, " $1").trim();
+        const result = test_name
+            .replace(/_([A-Z])/gi, (substring: string, match: string) => match.toUpperCase())
+            .replace(/([A-Z])/g, " $1")
+            .trim();
         return result.charAt(0).toUpperCase() + result.slice(1);
     }
 }
