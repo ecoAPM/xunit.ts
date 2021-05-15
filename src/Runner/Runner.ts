dd34a2a9153a9429e76b7664a6c9d11e197a4b82
Steve Desmond <steve@vtsv.ca>
2021-05-15 19:52:43 -0400

    Test run should fail if no tests found

--- src/Runner/Runner.ts
+++ src/Runner/Runner.ts
@@ -8,7 +8,7 @@ export default class Runner {
 
     constructor(private loader: TestSuiteLoader, private runner: TestSuiteRunner, private reporter: ResultReporter) { }
 
-    async runAll(dir: string) {
+    async runAll(dir: string): Promise<TestSuiteResults[]> {
         this.reporter.runStarted();
         const results: TestSuiteResults[] = [];
         const suites = await this.loader.loadTestSuites(dir);
@@ -21,7 +21,8 @@ export default class Runner {
         return results;
     }
 
-    static allTestsPassed(results: TestSuiteResults[]) {
-        return results.filter((result) => result.count(TestResult.Passed) < result.total()).length == 0;
+    static allTestsPassed(results: TestSuiteResults[]): boolean {
+        return results.filter(result => result.total() > 0).length > 0
+            && results.filter(result => result.count(TestResult.Passed) < result.total()).length === 0;
     }
 }
