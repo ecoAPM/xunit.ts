43b6a8badf6c06d57251a504ff9f4bdf126a06c1
evan-cohen <evancohen97@gmail.com>
2021-12-11 19:51:27 +0000

    refactor: use class name for filtering
    This eliminates the assumption that the file name is the same as the class name.

--- src/Runners/TestSuiteLoader.ts
+++ src/Runners/TestSuiteLoader.ts
@@ -22,16 +22,12 @@ export default class TestSuiteLoader {
     async loadSelectedTestSuites(dir: string, filter: string): Promise<Record<string, TestSuite>> {
         const files = (await this.file_system.getFiles(dir))
             .filter((file) => FileSystem.extension(file) === FileSystem.extension(__filename));
+        const [selectedSuite] = filter.split('.');
         const suites: Record<string, TestSuite> = {};
         for (const file of files) {
-            const [, relativePathToSuite] = file.split(dir + path.sep);
-            const suiteName = FileSystem.fileName(relativePathToSuite);
-            console.log(relativePathToSuite, suiteName);
-            if (!suiteName || !filter.startsWith(suiteName)) {
-                continue;
-            }
             const suite = await TestSuiteLoader.loadTestSuite(file);
-            if (suite !== undefined && suite !== null) {
+            const suiteName = suite?.constructor.name;
+            if (suite !== undefined && suite !== null && suiteName === selectedSuite) {
                 suites[file] = suite;
             }
         }
